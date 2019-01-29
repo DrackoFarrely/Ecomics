57a653ee60932172c6eea45c4a6e8aa5f279e473
Ivan Rios <iby_8_222@hotmail.com>
2019-01-29 10:52:48 -0600

    definicion de detalle

---
+++ WebEcomics/Models/Detalle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebEcomics.Models
+{
+    public class Detalle
+    {
+
+        public int id_compra { set; get; }
+
+        public int id_producto { set; get; }
+        public int Cantidad { set; get; }
+
+
+
+        public virtual ICollection<Productos> Productos { get; set; }
+        public virtual ICollection<Compras> Compras { get; set; }
+    }
+}
