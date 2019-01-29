9090d91898d3193c97a31668c53c671c87271698
Andy <Andy@DESKTOP-SJ062KB>
2019-01-29 11:39:05 -0600

    compra cambio2

--- WebEcomics/Models/Compra.cs
+++ WebEcomics/Models/Compra.cs
@@ -7,7 +7,15 @@ namespace WebEcomics.Models
 {
     public class Compra
     {
+
         public int Idcompra { get; set; }
+        public int Id_usuario { get; set; }
+        public DateTime fecha { get; set; }
         public double totalcompra { get; set; }
+
+        public virtual Usuario Usuario { get; set; }
+
+
+
     }
 }
